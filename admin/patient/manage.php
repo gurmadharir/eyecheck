65ffdb777c1f306765d22e02b93a73f610c7d579
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 22:16:55 +0300

    Universal theme added ....

--- admin/patient/manage.php
+++ admin/patient/manage.php
@@ -11,10 +11,15 @@ $page = 'patients';
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Patients | Admin</title>
-  <link rel="stylesheet" href="../../css/global.css" />
+
+  <!-- Theme bootstrap CSS -->
+  <script src="../js/theme-init.js"></script>
+
   <link rel="stylesheet" href="../../css/records.css" />
-  <link rel="stylesheet" href="../../css/theme.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../../css/global.css" />
 </head>
 <body>
 <div class="dashboard-wrapper">
@@ -84,8 +89,8 @@ $page = 'patients';
   <span id="toastMessage">Action complete!</span>
 </div>
 
-
-<script src="../../js/theme.js"></script>
+<script src="../../js/sidebar-toggle.js" defer></script>
+<script src="../../js/theme-toggle.js"></script>
 
 <script>
   window.adminRecordsConfig = {
