55d17d11282b7faee7d5785b67c72d55627d007d
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 08:24:48 +0300

    Changed welcome text

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -11,8 +11,8 @@
       <li><a href="/eyecheck/admin/dashboard.php" class="<?= ($page === 'dashboard' || $page === 'profile' || $page === 'create') ? 'active' : '' ?>">Dashboard</a></li>
       <li><a href="/eyecheck/admin/manage.php" class="<?= ($page === 'manage') ? 'active' : '' ?>">Management</a></li>
       <li><a href="/eyecheck/admin/patient/manage.php" class="<?= ($page === 'patients') ? 'active' : '' ?>">Patients</a></li>
-      <li><a href="/eyecheck/admin/manage-user-alerts.php" class="<?= ($page === 'user-alerts') ? 'active' : '' ?>">Alerts</a></li>
-      <li><a href="/eyecheck/admin/manage-logs.php" class="<?= ($page === 'logs') ? 'active' : '' ?>">Logs</a></li>
+      <li><a href="/eyecheck/admin/manage-user-alerts.php" class="<?= ($page === 'user-alerts') ? 'active' : '' ?>">User Alerts</a></li>
+      <li><a href="/eyecheck/admin/manage-logs.php" class="<?= ($page === 'logs') ? 'active' : '' ?>">User Logs</a></li>
 
 
     <?php elseif ($role === 'healthcare'): ?>
