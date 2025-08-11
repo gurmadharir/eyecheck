0e37d404384aa0840908f6184e2606bacf95b8de
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 22:21:21 +0300

    Universal theme applied ....

--- partials/auth/forgot-password-form.php
+++ partials/auth/forgot-password-form.php
@@ -1,5 +1,5 @@
 <?php if (!isset($_SESSION['show_reset_message'])): ?>
-  <form id="forgot-form" class="auth-form" style="width: 350px;" method="POST">
+  <form id="forgot-form" class="auth-form" style="max-width: 350px; padding: 30px;" method="POST">
     <h2 class="animated-text animated-forgot">Forgot your password?</h2>
     <input type="email" name="email" placeholder="Enter your email" required />
     <input type="hidden" name="role" value="<?php echo $role; ?>" />
