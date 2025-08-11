12b5f339c5e1b68a98cf3d5dd3e00cd1a5af08d0
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 22:20:41 +0300

    Added and applied universal theme....

--- healthcare/patients/upload.php
+++ healthcare/patients/upload.php
@@ -25,10 +25,15 @@ $existing_region = '';
 <html lang="en">
 <head>
   <title>New Patient | Healthcare</title>
-  <link rel="stylesheet" href="../../css/global.css" />
+  
+   <!-- Theme bootstrap CSS -->
+  <script src="../../js/theme-init.js"></script>
+
   <link rel="stylesheet" href="../../css/upload.css" />
-  <link rel="stylesheet" href="../../css/theme.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../../css/global.css" />
 </head>
 <body>
 <div class="dashboard-wrapper">
@@ -40,7 +45,8 @@ $existing_region = '';
     <?php include '../../partials/upload-form.php'; ?>
   </div>
 </div>
-<script src="../../js/theme.js"></script>
+<script src="../../js/theme-toggle.js"></script>
+<script src="../../js/sidebar-toggle.js" defer></script>
 <script src="../../js/upload.js"></script>
 </body>
 </html>
