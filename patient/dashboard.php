b4199ccc5097fe36b1ee6e74402bf130fb955d1f
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 22:21:54 +0300

    Universal theme applied ....

--- patient/dashboard.php
+++ patient/dashboard.php
@@ -8,10 +8,16 @@ $page = 'dashboard';
 <html lang="en">
 <head>
   <title>Dashboard | Patient</title>
-  <link rel="stylesheet" href="../css/global.css" />
-  <link rel="stylesheet" href="../css/theme.css" />
+
+  <!-- Theme bootstrap CSS -->
+  <script src="../js/theme-init.js"></script>
+
   <link rel="stylesheet" href="../css/dashboard.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/global.css" />
+
 </head>
 <body>
 
@@ -27,7 +33,8 @@ $page = 'dashboard';
 
 <script>const userRole = <?= json_encode($_SESSION['role']) ?>;</script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="../js/theme.js"></script>
+<script src="../js/theme-toggle.js" defer></script>
+<script src="../js/sidebar-toggle.js" defer></script>
 <script src="../js/dashboard/dashboard.js"></script>
 </body>
 </html>
