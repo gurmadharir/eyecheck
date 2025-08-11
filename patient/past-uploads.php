de4a200f6edb06135f3886354314a46eb35b867c
Gurmad Harir <gurmadharir@outlook.com>
2025-08-11 22:22:03 +0300

    Universal theme applied ....

--- patient/past-uploads.php
+++ patient/past-uploads.php
@@ -13,10 +13,15 @@ $page = 'past-uploads';
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>My Uploads | EyeCheck</title>
-  <link rel="stylesheet" href="../css/global.css" />
+
+  <!-- Theme bootstrap CSS -->
+  <script src="../js/theme-init.js"></script>
+
   <link rel="stylesheet" href="../css/records.css" />
-  <link rel="stylesheet" href="../css/theme.css" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/global.css" />
 </head>
 <body>
 
@@ -81,8 +86,8 @@ $page = 'past-uploads';
 
 <?php include '../partials/delete-toast.php'; ?>
 <script src="../js/delete-toast.js"></script>
-
-<script src="../js/theme.js"></script>
+<script src="../js/theme-toggle.js" defer></script>
+<script src="../js/sidebar-toggle.js" defer></script>
 <script src="../js/patient-uploads.js"></script>
 
 <script>
